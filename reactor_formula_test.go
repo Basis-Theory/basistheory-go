9666d3365634bdeb2e6734be15716dabcfd8ae05
Brian Gonzalez <brian.gonzalez@basistheory.com>
2021-10-10 10:32:10 -0600

    Finishes out tests for GetReactorFormulasWithQuery

--- reactor_formula_test.go
+++ reactor_formula_test.go
@@ -96,11 +96,51 @@ var _ = Describe("ReactorFormula client", func() {
 			})
 
 			Context("request to /reactor-formulas with query params is successful", func() {
+				It("returns PaginatedReactorFormulas", func() {
+					responder, _ := httpmock.NewJsonResponder(200, expectedPaginatedReactorFormulas)
+					params := map[string]string {
+						"name": expectedReactorFormulaQuery.Name,
+						"source_token_type": expectedReactorFormulaQuery.SourceTokenType,
+						"page": expectedReactorFormulaQuery.Page,
+						"size": expectedReactorFormulaQuery.Size,
+					}
+					path := fmt.Sprintf("%s/reactor-formulas?name=%s&page=%s&size=%s&source_token_type=%s",
+						baseUrl, params["name"], params["page"], params["size"], params["source_token_type"])
+					httpmock.RegisterResponder("GET", path, responder)
 
+					actualReactorFormulas, err := btClient.GetReactorFormulasWithQuery(expectedReactorFormulaQuery)
+
+					if err != nil {
+						Fail(fmt.Sprintf("GetReactorFormulasWithQuery failed: %v", err))
+					}
+
+					Expect(*actualReactorFormulas).To(Equal(expectedPaginatedReactorFormulas))
+				})
 			})
 
 			Context("request to /reactor-formulas with query params is not successful", func() {
+				It("returns an error containing details of the failed request", func() {
+					params := map[string]string {
+						"name": expectedReactorFormulaQuery.Name,
+						"source_token_type": expectedReactorFormulaQuery.SourceTokenType,
+						"page": expectedReactorFormulaQuery.Page,
+						"size": expectedReactorFormulaQuery.Size,
+					}
+					path := fmt.Sprintf("%s/reactor-formulas?name=%s&page=%s&size=%s&source_token_type=%s",
+						baseUrl, params["name"], params["page"], params["size"], params["source_token_type"])
+					expectedErrorPayload, expectedStatus := test_utils.SetupErrorPath("GET", path)
 
+					_, err := btClient.GetReactorFormulasWithQuery(expectedReactorFormulaQuery)
+
+					marshalledExpectedError, _ := json.Marshal(expectedErrorPayload)
+
+					Expect(*err.(*ApiError)).To(Equal(ApiError{
+						Code: expectedStatus,
+						Message: fmt.Sprintf(
+							"Sending GET request to %s: %d. Response body: %v",
+							path, expectedStatus, string(marshalledExpectedError)),
+					}))
+				})
 			})
 		})
 
