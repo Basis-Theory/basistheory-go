b532dca19d3e1a84c514800c7ebaa83f5c246852
Brian Gonzalez <brian.gonzalez@basistheory.com>
2022-04-05 13:48:03 -0600

    chore(ENG-2646): completes application CRUD tests

--- api_applications_test.go
+++ api_applications_test.go
@@ -34,9 +34,15 @@ func assertPropertiesMatch(actualProperty interface{}, expectedProperty interfac
 	}
 }
 
-func assertPropertiesMatchGeneric[T any](actualProperty interface{}, expectedProperty interface{}, t *testing.T, comparedType T) {
+func assertPropertiesMatchGeneric[T any](actualProperty interface{}, expectedProperty interface{}, comparedType T, t *testing.T) {
 	if !cmp.Equal(actualProperty, expectedProperty, cmp.AllowUnexported(comparedType)) {
 		t.Errorf("does not match expected: got: %+v, want: %+v", spew.Sdump(actualProperty), spew.Sdump(expectedProperty))
+	}
+}
+
+func assertDeletion[T any](actualDeletedResource T, expectedDeletedResource T, methodName string, t *testing.T) {
+	if !cmp.Equal(actualDeletedResource, expectedDeletedResource) {
+		t.Fatalf("%s failed to delete", methodName)
 	}
 }
 
@@ -63,12 +69,48 @@ func TestApplicationCRUD(t *testing.T) {
 	assertPropertiesMatchGeneric[NullableString](application.Name, NullableString{
 		value: &applicationName,
 		isSet: true,
-	}, t, NullableString{})
+	}, NullableString{}, t)
 	assertPropertiesMatchGeneric[NullableString](application.Type, NullableString{
 		value: &applicationType,
 		isSet: true,
-	}, t, NullableString{})
+	}, NullableString{}, t)
 	assertPropertiesMatch(application.Permissions, applicationPermissions, t)
 
-	// _, response, err = apiClient.ApplicationsApi.ApplicationsGet(contextWithAPIKey).Execute()
+	var applications *ApplicationModelPaginatedList
+	applications, response, err = apiClient.ApplicationsApi.ApplicationsGet(contextWithAPIKey).Execute()
+
+	assertMethodDidNotError(err, response, "ApplicationsGet", t)
+
+	assertPropertiesMatchGeneric[NullableString](applications.Data[0].Name, NullableString{
+		value: &applicationName,
+		isSet: true,
+	}, NullableString{}, t)
+	assertPropertiesMatchGeneric[NullableString](applications.Data[0].Type, NullableString{
+		value: &applicationType,
+		isSet: true,
+	}, NullableString{}, t)
+	assertPropertiesMatch(applications.Data[0].Permissions, applicationPermissions, t)
+
+	updatedApplicationName := "Updated Name"
+	updatedApplicationPermissions := []string{"token:general:read:low"}
+	var updatedApplication *ApplicationModel
+	updatedApplication, response, err = apiClient.ApplicationsApi.ApplicationUpdate(contextWithAPIKey, createdApplication.GetId()).UpdateApplicationModel(UpdateApplicationModel{
+		Name: NullableString{
+			value: &updatedApplicationName,
+			isSet: true,
+		},
+		Permissions: updatedApplicationPermissions,
+	}).Execute()
+
+	assertMethodDidNotError(err, response, "ApplicationUpdate", t)
+	assertPropertiesMatchGeneric[NullableString](updatedApplication.Name, NullableString{
+		value: &updatedApplicationName,
+		isSet: true,
+	}, NullableString{}, t)
+	assertPropertiesMatch(updatedApplication.Permissions, updatedApplicationPermissions, t)
+	response, err = apiClient.ApplicationsApi.ApplicationDelete(contextWithAPIKey, createdApplication.GetId()).Execute()
+	assertMethodDidNotError(err, response, "ApplicationDelete", t)
+	var deletedApplication *ApplicationModel
+	deletedApplication, _, _ = apiClient.ApplicationsApi.ApplicationGetById(contextWithAPIKey, createdApplication.GetId()).Execute()
+	assertDeletion[*ApplicationModel](deletedApplication, nil, "ApplicationDelete", t)
 }
