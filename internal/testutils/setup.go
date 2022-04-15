b643cb99df106db30cc7957e20722ac23c05615c
Adam Letizia <adam@basistheory.com>
2022-04-15 10:14:16 -0500

    fix port for mockserver

--- internal/testutils/setup.go
+++ internal/testutils/setup.go
@@ -8,7 +8,7 @@ import (
 func CreateLocalAPIAndContext() (*basistheory.APIClient, context.Context) {
 	configuration := basistheory.NewConfiguration()
 	configuration.Scheme = "http"
-	configuration.Host = "localhost:5090"
+	configuration.Host = "localhost:1080"
 
 	apiClient := basistheory.NewAPIClient(configuration)
 	contextWithAPIKey := context.WithValue(context.Background(), basistheory.ContextAPIKeys, map[string]basistheory.APIKey{
