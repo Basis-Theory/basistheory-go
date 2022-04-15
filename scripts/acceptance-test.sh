217b683451d83762f21728c093e33ea6fba9c28d
Brian Gonzalez <brian.gonzalez@basistheory.com>
2022-04-15 08:35:32 -0600

    chore: adds echo debug statements

--- scripts/acceptance-test.sh
+++ scripts/acceptance-test.sh
@@ -5,10 +5,14 @@ current_directory="$PWD"
 cd $(dirname $0)
 cd ../
 
+echo "running clean operation"
 go clean -testcache
+echo "running go tests"
 go test ./tests
 
 result=$?
+
+echo "result: $result"
 
 cd "$current_directory"
 
